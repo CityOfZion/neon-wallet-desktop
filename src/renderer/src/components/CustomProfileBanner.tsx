12cffe6030fc7f50fd189bbc69c2c262f91ad0f6
Raul Duarte Pereira <raulduartepe@gmail.com>
2024-06-18 17:39:33 -0300

    CU-86dtu95v5 - NEON3 - Network Configuration Profile - Behavior enhacement for Testnet outline and default profile

--- src/renderer/src/components/CustomProfileBanner.tsx
+++ src/renderer/src/components/CustomProfileBanner.tsx
@@ -1,26 +1,23 @@
 import { createPortal } from 'react-dom'
-import { useTranslation } from 'react-i18next'
-import { useSelectedNetworkByBlockchainSelector } from '@renderer/hooks/useSettingsSelector'
+import { DEFAULT_NETWORK_PROFILE } from '@renderer/constants/networks'
+import { useSelectedNetworkProfileSelector } from '@renderer/hooks/useSettingsSelector'
 
 const Banner = () => {
-  const { t: tCommon } = useTranslation('common', { keyPrefix: 'networkTypeLabel' })
-  const { networkByBlockchain } = useSelectedNetworkByBlockchainSelector()
+  const { selectedNetworkProfile } = useSelectedNetworkProfileSelector()
 
-  const hasTestnet = Object.values(networkByBlockchain).some(({ type }) => type === 'testnet')
-
-  if (!hasTestnet) return <></>
+  if (selectedNetworkProfile.id === DEFAULT_NETWORK_PROFILE.id) return <></>
 
   return (
     <div className="w-screen border-t-3 border-purple flex justify-center absolute top-drag-region left-0 z-50">
       <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
-      <span className="text-white text-1xs font-medium px-2.5 pb-1 pt-0.5 tracking-wide rounded-b-md bg-purple uppercase">
-        {tCommon('testnet')}
+      <span className="text-white text-xs px-2.5 pb-1 pt-0.5 tracking-wide rounded-b-md bg-purple uppercase">
+        {selectedNetworkProfile.name}
       </span>
       <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[-0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
     </div>
   )
 }
 
-export const TestnetBanner = () => {
+export const CustomProfileBanner = () => {
   return createPortal(<Banner />, document.getElementById('root')!)
 }
--- src/renderer/src/routes/pages/Settings/SettingsNetwork/BlockchainNetworkAccordion.tsx
+++ src/renderer/src/routes/pages/Settings/SettingsNetwork/BlockchainNetworkAccordion.tsx
@@ -2,8 +2,9 @@ import { useTranslation } from 'react-i18next'
 import { TBlockchainServiceKey } from '@renderer/@types/blockchain'
 import { Accordion } from '@renderer/components/Accordion'
 import { BlockchainIcon } from '@renderer/components/BlockchainIcon'
+import { DEFAULT_NETWORK_PROFILE } from '@renderer/constants/networks'
 import { useModalNavigate } from '@renderer/hooks/useModalRouter'
-import { useSelectedNetworkSelector } from '@renderer/hooks/useSettingsSelector'
+import { useSelectedNetworkProfileSelector, useSelectedNetworkSelector } from '@renderer/hooks/useSettingsSelector'
 
 import { BlockchainNetworkButton } from './BlockchainNetworkButton'
 
@@ -16,6 +17,10 @@ export const BlockchainNetworkAccordion = ({ blockchain }: TProps) => {
   const { t } = useTranslation('pages', { keyPrefix: 'settings.settingsNetwork' })
   const { modalNavigateWrapper } = useModalNavigate()
   const { network } = useSelectedNetworkSelector(blockchain)
+  const { selectedNetworkProfile } = useSelectedNetworkProfileSelector()
+
+  const isDefaultSelected = selectedNetworkProfile.id === DEFAULT_NETWORK_PROFILE.id
+
   return (
     <Accordion.Item value={blockchain}>
       <Accordion.Trigger>
@@ -36,7 +41,9 @@ export const BlockchainNetworkAccordion = ({ blockchain }: TProps) => {
               blockchain,
             },
           })}
+          disabled={isDefaultSelected}
         />
+
         <BlockchainNetworkButton
           className="border-none"
           label={t('nodeSelection')}
@@ -46,6 +53,7 @@ export const BlockchainNetworkAccordion = ({ blockchain }: TProps) => {
               blockchain,
             },
           })}
+          disabled={network.type === 'custom'}
         />
       </Accordion.Content>
     </Accordion.Item>
