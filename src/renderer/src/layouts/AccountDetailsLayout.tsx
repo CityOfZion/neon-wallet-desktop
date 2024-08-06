ed414d259b189b9ddee487604bffdcd569895951
Raul Duarte Pereira <raulduartepe@gmail.com>
2024-08-05 22:41:46 -0300

    CU-86du95ngj - NEON3 - Account Overview - Holdings value breaking line at $ in some higher value scenarios

--- src/renderer/src/layouts/AccountDetailsLayout.tsx
+++ src/renderer/src/layouts/AccountDetailsLayout.tsx
@@ -9,7 +9,10 @@ type TProps = {
 
 export const AccountDetailsLayout = ({ title, actions, children, className, ...props }: TProps): JSX.Element => {
   return (
-    <div className={StyleHelper.mergeStyles('w-full flex flex-col flex-grow px-4 py-3 min-h-0', className)} {...props}>
+    <div
+      className={StyleHelper.mergeStyles('w-full flex flex-col flex-grow px-4 py-3 min-h-0 min-w-0', className)}
+      {...props}
+    >
       <div className="flex justify-between items-center text-sm mb-3 max-h-[1.75rem] h-full">
         <h1 className="text-white text-sm">{title}</h1>
 
