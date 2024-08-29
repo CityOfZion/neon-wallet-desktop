7e7c74503b5c55a01110af20372abeeef3e6643a
Raul Duarte Pereira <raulduartepe@gmail.com>
2024-08-29 19:21:12 -0300

    CU-86duf5c4b - NEON3 - Adjust our implementation so we don't do a full refresh when focusing from idle

--- src/renderer/src/libs/query.ts
+++ src/renderer/src/libs/query.ts
@@ -3,7 +3,9 @@ import { QueryClient } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      retry: 0,
+      retry: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
       gcTime: Infinity,
       staleTime: 5 * 60 * 1000, // 5 minutes
     },
