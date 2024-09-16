f59e3daab0312624e76e8e091a2799bd617fdf3f
hotequil <hotequil@protonmail.com>
2024-09-16 18:25:35 -0300

    CU-86a4wpzuv-NEON3 - Wipe Local Data

---
+++ src/renderer/src/components/SuccessIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { TbDiscountCheck } from 'react-icons/tb'
+
+export const SuccessIcon: React.FC = () => (
+  <TbDiscountCheck
+    className={'text-blue stroke-1 bg-asphalt rounded-[50%] p-1.5 mt-8 w-[118px] h-[118px]'}
+    aria-hidden={true}
+  />
+)
