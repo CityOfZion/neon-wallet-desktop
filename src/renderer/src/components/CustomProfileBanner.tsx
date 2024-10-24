70e096e0bf76624d36f941d6d40b18a4cf04f785
hotequil <hotequil@protonmail.com>
2024-10-24 14:44:55 -0300

    CU-86a5b4q0v-NEON3 - BUG - When test network is connected, the user can't click on buttons in the top

--- src/renderer/src/components/CustomProfileBanner.tsx
+++ src/renderer/src/components/CustomProfileBanner.tsx
@@ -9,11 +9,13 @@ const Banner = () => {
 
   return (
     <div className="w-screen border-t-3 border-purple flex justify-center absolute top-drag-region left-0 z-50">
-      <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
-      <span className="text-white text-xs px-2.5 pb-1 pt-0.5 tracking-wide rounded-b-md bg-purple uppercase">
-        {selectedNetworkProfile.name}
-      </span>
-      <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[-0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
+      <div className="flex justify-center absolute">
+        <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
+        <span className="text-white text-xs px-2.5 pb-1 pt-0.5 tracking-wide rounded-b-md bg-purple uppercase block truncate max-w-[400px]">
+          {selectedNetworkProfile.name}
+        </span>
+        <div className="w-4.5 h-4.5 overflow-hidden relative before:content-[''] before:block before:w-[100%] before:h-[100%] before:absolute before:top-0 before:left-0 before:shadow-[-0.563rem_-0.563rem_0_0] before:shadow-purple before:rounded-[50%]" />
+      </div>
     </div>
   )
 }
