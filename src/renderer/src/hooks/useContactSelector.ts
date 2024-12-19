5cbe3a750c01288a2aeaeba6b4c108ca020e95e3
hotequil <hotequil@protonmail.com>
2024-12-18 22:42:19 -0300

    CU-86a5xqdbn-NEON3 - Swap Request - Limit destination accounts when token "TO" is from a supported network

--- src/renderer/src/hooks/useContactSelector.ts
+++ src/renderer/src/hooks/useContactSelector.ts
@@ -1,4 +1,6 @@
+import { useMemo } from 'react'
 import { ContactsHelper } from '@renderer/helpers/ContactsHelper'
+import { TBlockchainServiceKey } from '@shared/@types/blockchain'
 
 import { createAppSelector, useAppSelector } from './useRedux'
 
@@ -11,4 +13,16 @@ export const useContactsSelector = () => {
     contacts: value,
     contactsRef: ref,
   }
+}
+
+export const useHasContactsByBlockchain = (blockchain?: TBlockchainServiceKey) => {
+  const { contacts } = useContactsSelector()
+
+  const hasContactsByBlockchain = useMemo(() => {
+    if (!blockchain) return false
+
+    return contacts.flatMap(contact => contact.addresses.map(address => address.blockchain)).includes(blockchain)
+  }, [blockchain, contacts])
+
+  return { hasContactsByBlockchain }
 }
