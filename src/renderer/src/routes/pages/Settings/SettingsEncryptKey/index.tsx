6fbd8aba049c57067cbaa086b8bcbb9cb928d5fd
hotequil <hotequil@protonmail.com>
2025-04-29 11:55:25 -0300

    CU-86a5y83jq-NEON3 - Encrypt Key - It's possible to enter a different passphrase in the fields.

--- src/renderer/src/routes/pages/Settings/SettingsEncryptKey/index.tsx
+++ src/renderer/src/routes/pages/Settings/SettingsEncryptKey/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent } from 'react'
 import { useTranslation } from 'react-i18next'
 import { MdOutlineKey } from 'react-icons/md'
 import { Button } from '@renderer/components/Button'
+import { PasswordHelper } from '@renderer/helpers/PasswordHelper'
 import { ToastHelper } from '@renderer/helpers/ToastHelper'
 import { UtilsHelper } from '@renderer/helpers/UtilsHelper'
 import { useActions } from '@renderer/hooks/useActions'
@@ -13,59 +14,58 @@ import { TBlockchainServiceKey } from '@shared/@types/blockchain'
 import { SettingsEncryptInputStep } from './SettingsEncryptInputStep'
 import { SettingsEncryptKeySuccessContent } from './SettingsEncryptKeySuccessContent'
 
+type PassphraseField = 'passphrase' | 'confirmationPassphrase'
+
 type TFormData = {
   privateKey: string
   passphrase: string
   confirmationPassphrase: string
 }
 
-const MIN_LENGTH_PASSPHRASE = 4
-
 export const SettingsEncryptKeyPage = (): JSX.Element => {
   const { t } = useTranslation('pages', { keyPrefix: 'settings' })
   const { modalNavigate } = useModalNavigate()
 
-  const { handleAct, setError, actionState, actionData, setData, reset } = useActions<TFormData>({
+  const { handleAct, setError, actionState, actionData, setData, clearErrors, reset } = useActions<TFormData>({
     privateKey: '',
     passphrase: '',
     confirmationPassphrase: '',
   })
 
+  const validatePassphraseField = (
+    value: string,
+    passphraseField: PassphraseField,
+    comparablePassphraseField: PassphraseField
+  ) => {
+    setData({ [passphraseField]: value })
+
+    const trimmedValue = value.trim()
+    const comparablePassphraseValue = actionData[comparablePassphraseField]
+
+    if (trimmedValue.length < PasswordHelper.MINIMUM_PASSWORD_LENGTH)
+      setError(
+        passphraseField,
+        t(`encryptKey.error.${passphraseField}IsInvalid`, { chars: PasswordHelper.MINIMUM_PASSWORD_LENGTH })
+      )
+    else if (comparablePassphraseValue && value !== comparablePassphraseValue)
+      setError(passphraseField, t(`encryptKey.error.${passphraseField}IsDifferent`))
+    else clearErrors(comparablePassphraseField)
+  }
+
   const handlePrivateKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = UtilsHelper.removeSpecialCharacters(event.target.value, { allowSpaces: false })
 
-    setData({
-      privateKey: value,
-    })
+    setData({ privateKey: value })
 
-    if (!bsAggregator.validateKeyAllBlockchains(value)) {
-      setError('privateKey', t('encryptKey.error.privateKey'))
-      return
-    }
+    if (!bsAggregator.validateKeyAllBlockchains(value)) setError('privateKey', t('encryptKey.error.privateKey'))
   }
 
   const handlePassphraseChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value
-    setData({
-      passphrase: value,
-    })
-
-    if (value.length < MIN_LENGTH_PASSPHRASE) {
-      setError('passphrase', t('encryptKey.error.passphrase'))
-      return
-    }
+    validatePassphraseField(event.target.value, 'passphrase', 'confirmationPassphrase')
   }
 
   const handleConfirmationPassphraseChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value
-    setData({
-      confirmationPassphrase: value,
-    })
-
-    if (actionData.passphrase !== value) {
-      setError('confirmationPassphrase', t('encryptKey.error.confirmationPassphrase'))
-      return
-    }
+    validatePassphraseField(event.target.value, 'confirmationPassphrase', 'passphrase')
   }
 
   const handleSelect = async (blockchain: TBlockchainServiceKey) => {
@@ -78,7 +78,7 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
       modalNavigate('success', {
         state: {
           heading: t('encryptKey.successModal.title'),
-          headingIcon: <MdOutlineKey />,
+          headingIcon: <MdOutlineKey aria-hidden={true} />,
           content: <SettingsEncryptKeySuccessContent encryptedKey={encryptedKey} />,
           subtitle: t('encryptKey.successModal.subtitle'),
         },
@@ -94,7 +94,7 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
     modalNavigate('blockchain-selection', {
       state: {
         heading: t('encryptKey.successModal.title'),
-        headingIcon: <MdOutlineKey />,
+        headingIcon: <MdOutlineKey aria-hidden={true} />,
         withBackButton: false,
         onSelect: handleSelect,
       },
@@ -104,8 +104,8 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
   return (
     <SettingsLayout title={t('securityOption.encryptKey')}>
       <form className="flex flex-col items-center text-xs flex-grow" onSubmit={handleAct(handleSubmit)}>
-        <div className="max-w-[22.5rem] w-full flex flex-grow flex-col">
-          <span className="text-gray-100 text-center mb-6">{t('encryptKey.subtitle')}</span>
+        <div className="max-w-[24rem] w-full flex flex-grow flex-col">
+          <h2 className="text-gray-100 text-center mb-6">{t('encryptKey.subtitle')}</h2>
 
           <div className="flex flex-col gap-4">
             <SettingsEncryptInputStep
@@ -113,9 +113,9 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
               pastable
               description={t('encryptKey.titleInput1')}
               placeholder={t('encryptKey.inputPrivateKeyPlaceholder')}
-              onChange={handlePrivateKeyChange}
               value={actionData.privateKey}
               errorMessage={actionState.errors.privateKey}
+              onChange={handlePrivateKeyChange}
             />
 
             <SettingsEncryptInputStep
@@ -123,20 +123,20 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
               description={t('encryptKey.titleInput2')}
               placeholder={t('encryptKey.inputPassphrasePlaceholder')}
               type="password"
-              onChange={handlePassphraseChange}
               value={actionData.passphrase}
               errorMessage={actionState.errors.passphrase}
+              onChange={handlePassphraseChange}
             />
 
             <SettingsEncryptInputStep
               step={3}
-              description={t('encryptKey.titleInput2')}
+              description={t('encryptKey.titleInput3')}
               placeholder={t('encryptKey.inputConfirmPassphrasePlaceholder')}
-              onChange={handleConfirmationPassphraseChange}
               type="password"
               value={actionData.confirmationPassphrase}
               errorMessage={actionState.errors.confirmationPassphrase}
               withLine={false}
+              onChange={handleConfirmationPassphraseChange}
             />
           </div>
         </div>
@@ -147,7 +147,7 @@ export const SettingsEncryptKeyPage = (): JSX.Element => {
           label={t('encryptKey.buttonGenerate')}
           loading={actionState.isActing}
           disabled={!actionState.isValid}
-          leftIcon={<MdOutlineKey />}
+          leftIcon={<MdOutlineKey aria-hidden={true} />}
         />
       </form>
     </SettingsLayout>
